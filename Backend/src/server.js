bf74a80ce9d3f13bbd551682b9d142b920bfa100
Farhan Ahmad <farhanahmadptn@gmail.com>
2025-01-11 15:22:17 +0530

    resolve fetching problem

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import fetchCryptoData from "./jobs/Background.js";
+import cryptoRoute from "./routes/cryptoRoute.js"
 
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 
+// Routes
+app.use("/api", cryptoRoute);
+
 const PORT = process.env.PORT || 5001;
+
+setInterval(async () => {
+  console.log("Fetching cryptocurrency data...");
+  try {
+    await fetchCryptoData();
+    console.log("Data fetched successfully.");
+  } catch (error) {
+    console.error("Error in scheduled task:", error.message);
+  }
+}, 2 * 60 * 60 * 1000); 
+
 
 
 app.listen(PORT, () => {
-  console.log("server running on port " + PORT)
+  console.log("server running on port " + PORT);
   connectDB();
 });
